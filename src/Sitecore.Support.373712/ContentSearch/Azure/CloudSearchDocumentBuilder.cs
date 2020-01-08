f7431ff38857ac2b031b824029316295e02c6dfe
Andrey Filchenkov <22079019+AndreyFilchenkov@users.noreply.github.com>
2020-01-08 17:32:18 +0200

    Import source code: CloudSearchDocumentBuilder

---
+++ src/Sitecore.Support.373712/ContentSearch/Azure/CloudSearchDocumentBuilder.cs
@@ -0,0 +1,60 @@
+﻿namespace Sitecore.Support.ContentSearch.Azure
+{
+    using Sitecore.ContentSearch;
+    using System;
+    using System.Collections.Concurrent;
+    using System.Globalization;
+
+    public class CloudSearchDocumentBuilder : Sitecore.ContentSearch.Azure.CloudSearchDocumentBuilder
+    {
+        private readonly CultureInfo culture;
+
+        public CloudSearchDocumentBuilder(IIndexable indexable, IProviderUpdateContext context)
+        : base(indexable, context)
+        {
+            culture = indexable?.Culture;
+        }
+
+        protected override void AddItemFields()
+        {
+            try
+            {
+                VerboseLogging.CrawlingLogDebug(() => "AddItemFields start");
+                if (Options.IndexAllFields)
+                {
+                    Indexable.LoadAllFields();
+                }
+                if (IsParallel)
+                {
+                    ConcurrentQueue<Exception> exceptions = new ConcurrentQueue<Exception>();
+                    ParallelForeachProxy.ForEach(Indexable.Fields, ParallelOptions, delegate (IIndexableDataField f)
+                    {
+                        try
+                        {
+                            CheckAndAddField(Indexable, f);
+                        }
+                        catch (Exception item)
+                        {
+                            exceptions.Enqueue(item);
+                        }
+                    });
+                    if (exceptions.Count > 0)
+                    {
+                        throw new AggregateException(exceptions);
+                    }
+                }
+                else
+                {
+                    foreach (IIndexableDataField field in Indexable.Fields)
+                    {
+                        CheckAndAddField(Indexable, field);
+                    }
+                }
+            }
+            finally
+            {
+                VerboseLogging.CrawlingLogDebug(() => "AddItemFields End");
+            }
+        }
+    }
+}
